2a8dfacda741e81de0ab668d7084f6b21cc33c75
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-01 16:47:18 -0300

    Componente com Arrow Function

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,10 +1,17 @@
 //como terá jsx é importante importar o React
 import React from 'react'
 
+//Com Arrow Function
+export default () => 
+    <div>
+        <h1>Primeiro Componente (Arrow)!</h1>
+    </div>
+
+
 //Pode exportar uma função anonima se quiser
-export default function (){
-    return <h1>Primeiro Componente!</h1>
-}
+// export default function (){
+//     return <h1>Primeiro Componente!</h1>
+// }
 
 //function primeiro(){
 //    return <h1>Primeiro Componente!</h1>
