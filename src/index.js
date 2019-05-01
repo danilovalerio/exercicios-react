512dc07a69250dd74c9eaa2609eaf17eb06d9046
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-01 17:08:23 -0300

    Multiplos componentes num único arquivo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//importando o componente novo
-import PrimeiroComponente from './componentes/PrimeiroComponente'
+//importando o componentes
+//import PrimeiroComponente from './componentes/PrimeiroComponente'
+
+import {CompA, CompB as B} from './componentes/MultiplosComponentes'
 
 const elemento = document.getElementById('root')
 //passa a string para o elemento
 ReactDOM.render(
     <div>
-        <PrimeiroComponente valor="Bem vindo as props!" abcd={12345}></PrimeiroComponente>
+        <div>
+            <CompA valor="Sou o CompA"></CompA>
+            <B valor="Sou o CompB"></B>
+        </div>
+        <div>
+            {/* <PrimeiroComponente valor="Bem vindo as props!" abcd={12345}></PrimeiroComponente> */}
+        </div>
     </div>    
 , elemento)
