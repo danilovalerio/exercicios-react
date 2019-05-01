d90d83461f328c7e6427407ec2f910074e9db0bf
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-01 20:55:41 -0300

    Contador com o problema do this resolvido, agora a propriedade numero é somente leitura, próximo problema a ser solucionado

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,12 +1,13 @@
 import React, {Component } from 'react'
 
 export default class Contador extends Component{
+   
 
-    maisUm(){
-        alert("Função chamada topis!")
-        this.props.numero++
+    maisUm = () => {
+        // this.props.numero++
+        console.log(this)
     }
-
+    
     render(){
         return(
             <div>
@@ -16,5 +17,22 @@ export default class Contador extends Component{
             </div>
         )
     }
-
+    
 }
+
+//Solucão 01 - Para o problema de this undefined Bind
+//no constructor é passado o this e garantindo 
+//que this sempre será uma instância de contador por conta do bind
+// constructor(props){
+//     super(props)
+//     this.maisUm = this.maisUm.bind(this)
+// }
+
+//Solução 02 - com Arrow Function (onClick)
+// <button onClick={() => this.maisUm()}> + Inc</button>
+
+//Solução 03 Arrow Function no método, o this associa o local Contador
+// maisUm = () => {
+//     // this.props.numero++
+//     console.log(this)
+// }
